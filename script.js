cff76c20eaf75af24b388c174716d083d69ea8a2
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2021-11-26 14:38:01 +0100

    Render menu instead

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-let menuOpen = false
+const menuBtn = document.querySelector('#hamburger');
 
-function mobileMenu() {
-    if (menuOpen === false) {
-        document.querySelector('.mobile-menu').style.display = "block";
-        menuOpen = true;
-    } else if (menuOpen === true) {
-        document.querySelector('.mobile-menu').style.display = "none";
-        menuOpen = false;
+// Render background & menu for mobile
+menuBtn.addEventListener("click", () => {
+    const menuBackground = document.createElement('div');
+    menuBackground.className = 'menu-background';
+    document.body.appendChild(menuBackground);
+
+    const mobileMenu = document.createElement('div');
+    mobileMenu.className = 'mobile-menu';
+    document.body.appendChild(mobileMenu);
+    mobileMenu.innerHTML = `
+        <ul>
+            <img id="closeHamburger" src="img/close.png" alt="close mobile menu">
+            <li><a href="#playonline">Play online</a></li>
+            <li><a href="#playonsite">Play on-site</a></li>
+            <li><a href="#thestory">The story</a></li>
+            <li><a href="#contactus">Contact us</a></li>
+        </ul>
+    `;
+
+    // Removes the mobile menu and background
+    if(document.querySelector('#closeHamburger')){
+        const closeMenuBtn = document.querySelector('#closeHamburger');
+        closeMenuBtn.addEventListener("click", () => {
+            mobileMenu.remove()
+            menuBackground.remove()
+        })
     }
-}
+})
